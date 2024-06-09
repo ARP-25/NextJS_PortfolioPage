0bb0e7408e2e5b7e2923605b648fbda708bcb4fb
Angelo <angelo.pucci@outlook.de>
2024-06-09 14:11:02 +0200

    Bugfix: Title and Description in bentogrid now visible on smaller screen sizes

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -123,12 +123,12 @@ export const BentoGridItem = ({
                     )}
                 >
                     {/* change the order of the title and des, font-extralight, remove text-xs text-neutral-600 dark:text-neutral-300 , change the text-color */}
-                    <div className="font-sans font-extralight md:max-w-32 md:text-xs xl:text-base text-sm text-[#C1C2D3] z-10">
+                    <div className="font-sans font-extralight md:max-w-32 md:text-xs xl:text-base text-sm text-[#C1C2D3] z-50">
                         {description}
                     </div>
                     {/* add text-3xl max-w-96 , remove text-neutral-600 dark:text-neutral-300*/}
                     {/* remove mb-2 mt-2 */}
-                    <div className={`font-sans text-lg xl:text-3xl max-w-96 font-bold z-10`}>
+                    <div className={`font-sans text-lg xl:text-3xl max-w-96 font-bold z-50`}>
                         {title}
                     </div>
 
