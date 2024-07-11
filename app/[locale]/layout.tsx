b4e3a2846a59f76c36bd39ab974881a74df01c65
Angelo <angelo.pucci@outlook.de>
2024-07-11 22:57:28 +0200

    Avoid forced refresh on pageload

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { appWithTranslation } from 'next-i18next';
+import { appWithTranslation } from "next-i18next";
 import { ThemeProvider } from "./provider";
 
-import {NextIntlClientProvider} from 'next-intl';
-import {getMessages} from 'next-intl/server';
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,28 +13,37 @@ export const metadata: Metadata = {
     title: "Angelo's Website",
     description: "Modern & Minimalist Portfolio",
     icons: {
-        icon: ["https://cdn.iconscout.com/icon/free/png-256/free-rocket-launch-startup-business-mission-space-marketing-4-5432.png"]
-    }
+        icon: [
+            "https://cdn.iconscout.com/icon/free/png-256/free-rocket-launch-startup-business-mission-space-marketing-4-5432.png",
+        ],
+    },
 };
 
 export default async function RootLayout({
     children,
-    params: {locale}
-  }: {
+    params: { locale },
+}: {
     children: React.ReactNode;
-    params: {locale: string};
-  }) {
-
+    params: { locale: string };
+}) {
     // Providing all messages to the client
     // side is the easiest way to get started
     const messages = await getMessages();
 
     return (
         <html lang="en">
+            <head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+            </head>
             <body className={inter.className}>
                 {" "}
                 <NextIntlClientProvider messages={messages}>
-                    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+                    <ThemeProvider
+                        attribute="class"
+                        defaultTheme="dark"
+                        enableSystem
+                        disableTransitionOnChange
+                    >
                         {children}
                     </ThemeProvider>
                 </NextIntlClientProvider>
--- components/ui/DropdownMenuRadioGroupDemo.tsx
+++ components/ui/DropdownMenuRadioGroupDemo.tsx
@@ -1,68 +1,72 @@
 "use client";
-import { useTranslations } from 'next-intl';
+import { useTranslations } from "next-intl";
 import { TfiWorld } from "react-icons/tfi";
 import * as React from "react";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuLabel,
-  DropdownMenuRadioGroup,
-  DropdownMenuRadioItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuLabel,
+    DropdownMenuRadioGroup,
+    DropdownMenuRadioItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { FiFlag } from "react-icons/fi";
 
 export function LanguageDropdownMenu() {
-  const [language, setLanguage] = React.useState<string>("");
-  const router = useRouter();
+    const [language, setLanguage] = React.useState<string>("");
+    const router = useRouter();
 
-  const t = useTranslations("Languages");
+    const t = useTranslations("Languages");
 
-  // Retrieve the language from localStorage when the component mounts
-  React.useEffect(() => {
-    const storedLanguage = localStorage.getItem("selectedLanguage");
-    if (storedLanguage) {
-      setLanguage(storedLanguage);
-    } else {
-      // Set default language if no language is stored
-      const defaultLanguage = "en"; // Set your default language here
-      setLanguage(defaultLanguage);
-      localStorage.setItem("selectedLanguage", defaultLanguage);
-      router.push(`/${defaultLanguage}`);
-    }
-  }, []);
-  
-  // Watch for changes to the language state and navigate
-  React.useEffect(() => {
-    if (language) {
-      localStorage.setItem("selectedLanguage", language);
-      router.push(`/${language}`);
-    }
-  }, [language]);
+    // Retrieve the language from localStorage when the component mounts
+    React.useEffect(() => {
+        const storedLanguage = localStorage.getItem("selectedLanguage");
+        if (storedLanguage) {
+            setLanguage(storedLanguage);
+        } else {
+            // Set default language if no language is stored
+            const defaultLanguage = "de"; // Set your default language here
+            setLanguage(defaultLanguage);
+            localStorage.setItem("selectedLanguage", defaultLanguage);
+            // router.push(`/${defaultLanguage}`);
+        }
+    }, []);
 
-  return (
-    <div className="fixed top-4 right-4" style={{zIndex: 5000}}>
-      <div className="relative ">
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="outline" className="rounded-xl">
-              {language === "en" ? t("lang1") : t("lang2")}
-              <TfiWorld className="pl-2 transition duration-200 text-white hover:text-gray-300 bg-transparent text-2xl" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-56 rounded-xl">
-            <DropdownMenuLabel>{language === "en" ? "Choose your language" : "Wähle deine Sprache"}</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuRadioGroup value={language} onValueChange={setLanguage}>
-              <DropdownMenuRadioItem value="en">English / EN</DropdownMenuRadioItem>
-              <DropdownMenuRadioItem value="de" className="rounded-b-[8px]">Deutsch / DE</DropdownMenuRadioItem>
-            </DropdownMenuRadioGroup>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
-    </div>
-  );
+    // Watch for changes to the language state and navigate
+    React.useEffect(() => {
+        if (language) {
+            localStorage.setItem("selectedLanguage", language);
+            router.push(`/${language}`);
+        }
+    }, [language]);
+
+    return (
+        <div className="fixed right-4 top-4" style={{ zIndex: 5000 }}>
+            <div className="relative">
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="outline" className="rounded-xl">
+                            {language === "en" ? t("lang1") : t("lang2")}
+                            <TfiWorld className="bg-transparent pl-2 text-2xl text-white transition duration-200 hover:text-gray-300" />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="w-56 rounded-xl">
+                        <DropdownMenuLabel>
+                            {language === "en" ? "Choose your language" : "Wähle deine Sprache"}
+                        </DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuRadioGroup value={language} onValueChange={setLanguage}>
+                            <DropdownMenuRadioItem value="en">English / EN</DropdownMenuRadioItem>
+                            <DropdownMenuRadioItem value="de" className="rounded-b-[8px]">
+                                Deutsch / DE
+                            </DropdownMenuRadioItem>
+                        </DropdownMenuRadioGroup>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
+        </div>
+    );
 }
